f3e4b49d686a6a78e7ca7116ef762b252da61e6f
Daniel Parulian Nainggolan <100332267+danielpnainggolan@users.noreply.github.com>
2023-01-06 21:13:47 +0700

    Update Solutions.sql

--- Case Study #4 - Data Bank/Solutions.sql
+++ Case Study #4 - Data Bank/Solutions.sql
@@ -1,1 +1,35 @@
+#A. Customer Nodes Exploration 
+#number 1
+select count(distinct node_id) total_nodes from customer_nodes;
+
+#number 2
+select region_id, count(distinct node_id) total_node
+from customer_nodes
+group by 1;
+
+#number 3
+select region_id, count(distinct customer_id) total_customer
+from customer_nodes
+group by 1;
+
+#number 4
+with urutan as (
+select distinct customer_id,node_id, start_date, row_number () over( partition by customer_id, node_id order by start_date asc) urut
+from customer_nodes
+order by 1),
+
+selisih as(
+select customer_id, node_id, start_date, lag(start_date) over(partition by customer_id) dated
+from urutan
+where urut=1),
+
+average as (
+select *, abs(datediff(start_date,dated)) diff
+from selisih
+having diff is not NULL)
+
+select concat(round(avg(diff),2)," days") averages
+from average;
+
+#number 5
 
