90f14412bce88cc3d6b3016c3cd5f4df122b8057
Daniel Parulian Nainggolan <100332267+danielpnainggolan@users.noreply.github.com>
2023-01-30 21:49:55 +0700

    Update Solutions.sql

--- Case Study #4 - Data Bank/Solutions.sql
+++ Case Study #4 - Data Bank/Solutions.sql
@@ -33,3 +33,30 @@ from average;
 
 #number 5
 
+
+#B. Customer Transactions
+#number 1
+select txn_type, count(distinct txn_amount) unique_value, sum(txn_amount) total
+from customer_transactions
+group by 1;
+
+
+#number 2
+select count(txn_type)/(select count(*) from customer_transactions) average, sum(txn_amount) total
+from customer_transactions
+where txn_type ='deposit';
+
+
+#number 3
+with df as(
+select month(txn_date) month, customer_id, txn_date, txn_type,
+row_number() over(partition by customer_id order by txn_date) ff
+from customer_transactions
+where txn_type ='deposit' and customer_id in (select distinct customer_id 
+from customer_transactions 
+where txn_type in ('pirchase','withdrawal'))
+order by 1,2 asc)
+
+select distinct month, customer_id
+from df
+where ff>1
