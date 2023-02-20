24a3c40f7220a540f308d6701819ea84bf96c282
Daniel Parulian Nainggolan <100332267+danielpnainggolan@users.noreply.github.com>
2023-02-20 20:52:17 +0700

    Update Solutions.sql

--- Case Study #5 - Data Mart/Solutions.sql
+++ Case Study #5 - Data Mart/Solutions.sql
@@ -1,4 +1,4 @@
-#data cleaning
+#1. Data Cleansing Steps
 
 insert into clean_weekly_sales(
 select date(ft.tanggal) week_date,case
@@ -26,3 +26,52 @@ select *,concat(concat("20",right(format_waktu,2)),"-",substring(format_waktu,4,
 from(
 select *,date_format(week_date,"%y-%m-%d") format_waktu
 from weekly_sales) ws) ft)
+
+
+#2. Data Exploration
+
+#number 3
+select calender_year year,count(transactions) amount from clean_weekly_sales
+group by 1
+order by 1 asc;
+
+
+#number 4
+select month_number, region, sum(sales) amount
+from clean_weekly_sales
+group by 1,2
+order by 1 asc;
+
+
+#number 5
+select platform, sum(transactions) amount
+from clean_weekly_sales
+group by 1;
+
+
+#number 6
+select platform, round(100*(sum(sales)/(select sum(sales) from clean_weekly_sales)),2) percentage
+from clean_weekly_sales
+group by 1;
+
+
+#number 7
+select calender_year,demographic, round(100*(sum(sales)/(select sum(sales) from clean_weekly_sales)),2) percentage
+from clean_weekly_sales
+group by 1,2
+order by 1 asc;
+
+
+#number 8
+select age_band,demographic ,sum(sales) amount
+from clean_weekly_sales
+where platform ='Retail'
+group by 1,2
+order by 3 desc
+limit 1;
+
+
+#number 9
+select calender_year, platform, round(avg(transactions),2) amount
+from clean_weekly_sales
+group by 1,2;
