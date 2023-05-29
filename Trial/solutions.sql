545cc7fea175f5bf65cb6aa5bca4605ddf71ac8f
Daniel Parulian Nainggolan <100332267+danielpnainggolan@users.noreply.github.com>
2023-05-29 21:20:00 +0700

    Update solutions.sql

--- Trial/solutions.sql
+++ Trial/solutions.sql
@@ -1,1 +1,12 @@
 
+
+with part as(
+select *, dense_rank() over(partition by order_id order by date) ranking, 
+lag(date) over(partition by order_id order by date) as dated
+from order_log)
+
+select if(status='processed','cteeated to processed','processed to delivered') status,  
+       avg(datediff(date,dated)) average 
+from part
+where dated is not null
+group by 1;
